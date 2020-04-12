2b8389e979abeb6ba101b04f14973ab8337106bd
Michał Duda <this.michalduda@gmail.com>
2020-04-12 17:23:58 +0200

    Added Header + About WIP

---
+++ src/components/SectionAbout.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="about">
+    <div class="row">
+      <div class="col-sm-7 about__visual-wrapper">
+        <div class="about__visual"></div>
+      </div>
+      <div class="col-sm-6 offset-1 about__text-wrapper">
+        <h1 class="about__header">Podejmujemy wyzwanie cyfrowej medycyny.</h1>
+        <p>Dostarczamy dedykowane systemy informatyczne stworzone z myślą o ludzkiej stronie cyfrowej medycyny. Stawiamy na estetykę i funkcjonalność.</p>
+        <p>Nasze produkty są zaprojektowane z dbałością o komfort i łatwość użytkowania: intuicyjne i zintegrowane.</p>
+        <p>Łatwe w użyciu dla pacjenta i specjalistów. Pozwalają zdigitalizować usługę zachowując jej indywidualny charakter spotkań.</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.about__visual-wrapper {
+  background: $color-primary;
+  display: flex;
+  flex-direction: row-reverse;
+  &:before{
+    content: '';
+    display: block;
+    background: white;
+    width: 7.143%;
+  }
+}
+.about__visual{
+  background: url('~@/assets/about-visual.png');
+  mix-blend-mode: screen;
+  background-size: cover;
+  background-position: right top;
+  width: 50%;
+}
+.about__text-wrapper {
+}
+.about__header {
+  font-size: 28px;
+}
+</style>
---
+++ src/components/SectionHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="main-header">
+    <div class="row main-header__wrapper">
+      <div class="col-sm-2 offset-1">
+        <img class="main-header__logo" src="@/assets/taurusmed-logo.png" alt />
+      </div>
+      <div class="col-sm-11">
+        <p class="main-header__claim">profesjonalne systemy informatyczne</p>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.main-header {
+  &:before {
+    content: '';
+    display: block;
+    background: $color-primary;
+    height: 25px;
+  }
+}
+.main-header__logo {
+  width: 100%;
+}
+.main-header__wrapper {
+  min-height: 65px;
+  display: flex;
+  align-items: center;
+}
+.main-header__claim {
+  margin-left: 70px;
+}
+</style>
