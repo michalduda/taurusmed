c6fe9cab0291801443cef68789a45917599c9d64
Michał Duda <this.michalduda@gmail.com>
2020-05-09 19:40:38 +0200

    Added mobile menu

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,24 +1,33 @@
 <template>
   <header class="main-header">
-    <div class="row main-header__wrapper">
-      <div class="col-5 col-sm-2 offset-1">
+    <div class="row justify-content-between justify-content-md-start main-header__wrapper">
+      <div class="col-logo col-5 offset-1 col-sm-4 col-md-3 col-lg-2">
         <img
           class="main-header__logo"
           src="@/assets/taurusmed-logo.svg"
           alt
         />
       </div>
-      <div class="col-8 col-sm-11">
+      <div class="col-claim col-12 offset-1 col-sm-11 col-md-8 offset-md-0">
         <p class="main-header__claim">
           profesjonalne systemy informatyczne
         </p>
       </div>
+      <div class="col-hamburger col-auto d-md-none">
+        <header-hamburger />
+      </div>
     </div>
+
   </header>
 </template>
 
 <script>
-export default {}
+import HeaderHamburger from '@/components/header/HeaderHamburger'
+export default {
+  components: {
+    HeaderHamburger
+  }
+}
 </script>
 
 <style lang="scss">
@@ -30,6 +39,18 @@ export default {}
     height: 25px;
   }
 }
+.col-logo{}
+
+.col-claim{
+  order: 1
+}
+.col-hamburger{
+  margin-right: 7.14286%;
+  text-align: right;
+  .hamburger{
+    margin-right: -15px;
+  }
+}
 .main-header__logo {
   width: 100%;
 }
@@ -37,8 +58,27 @@ export default {}
   min-height: 65px;
   display: flex;
   align-items: center;
+  margin: 10px 0;
+  @include sm{
+    margin: 20px 0;
+  }
+  @include md{
+    min-height: 0;
+    align-items: baseline;
+    margin: 50px 0;
+  }
 }
 .main-header__claim {
-  margin-left: 5.85%;
+  // margin-left: 5.85%;
+  margin-top: 0;
+  font-size: .8em;
+  @media (min-width: 460px){
+    font-size: 1em;
+  }
+  @include md{
+    margin-top: 0;
+    margin-bottom: 0;
+    margin-left: 5.85%;
+  }
 }
 </style>
